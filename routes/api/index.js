f0516a2dcc2173e73f580a718e17babf947e8b63
Payton Whinnery <paytoncaliw@gmail.com>
2021-04-25 19:28:39 -0500

    adding starter code and got server working

---
+++ routes/api/index.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const path = require("path");
+const {route, put } = require("..");
+
+// router.get("/api/workouts", ({ body }, res) => {
+    
+// });
+
+// router.post("/api/workouts", ({ body }, res) => {
+
+// });
+// // get all
+// route.get /api/workouts;
+// post /api/workouts
+// // get one
+// api/workouts/:id
+
+// api/workout/range 
+
+// /exercise
+
+// put('api/workouts/:id'
+
+// html 
+// display exercise.html
+// Stats.js
