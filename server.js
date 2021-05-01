a378e7c828431885c6c87440aa69d2796b86ab59
Payton Whinnery <paytoncaliw@gmail.com>
2021-05-01 15:40:14 -0500

    pushing

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WorkoutDB", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // routes
